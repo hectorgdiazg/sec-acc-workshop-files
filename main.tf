db5e7500298a03b7a2ae54ce38b36252c4b6e933
asjadnasir <58575332+asjadnasir@users.noreply.github.com>
2020-04-18 21:41:00 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,7 @@
 resource "google_compute_instance" "default" {
-  name         = "test"
+  name         = "terraform-test-vm"
   machine_type = "n1-standard-1"
   zone         = "us-central1-a"
-
-  tags = ["foo", "bar"]
 
   boot_disk {
     initialize_params {
@@ -24,13 +22,13 @@ resource "google_compute_instance" "default" {
     }
   }
 
-  metadata = {
-    foo = "bar"
-  }
-
   metadata_startup_script = "echo hi > /test.txt"
 
   service_account {
     scopes = ["userinfo-email", "compute-ro", "storage-ro"]
   }
+}
+resource "google_storage_bucket" "default" {
+  name     = "ce-accel-test-bucket"
+  storage_class = "MULTI_REGIONAL"
 }
